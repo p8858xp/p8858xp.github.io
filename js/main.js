e5a021a664802e4235c8d048a2985110a6b3f5c0
Paul Park <paulpark@Pauls-Air-2.home>
2018-11-11 17:11:53 -0500

    updated and good to go

--- js/main.js
+++
@@ -1,6 +0,0 @@
-/*
-const backgroundReq = new XMLHttpRequest();
-backgroundReq.open('GET', "http://localhost:3000/about", true);
-backgroundReq.addEventListener('load', parseResponse);
-backgroundReq.send();
-*/
